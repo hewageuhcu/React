0e40c8326ac15c1bb6ccb7d97c0f222401c4b8d6
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-01-05 22:52:36 +0530

    Add files via upload

--- Contact.js
+++ Contact.js
@@ -23,11 +23,14 @@ function Contact() {
   <div>
   <div class="column1">
 <img src="https://cdn-icons-png.flaticon.com/128/13/13936.png" heigh="60px" width="60px"></img>
-<img src="https://cdn-icons-png.flaticon.com/128/900/900310.png" heigh="60px" width="60px"></img>
-<img src="https://cdn-icons-png.flaticon.com/128/2165/2165061.png"heigh="60px" width="60px"></img>
-   
+<br/>
 
-<img src="https://cdn-icons-png.flaticon.com/128/535/535239.png" heigh="60px" width="60px"></img>
+<img src="https://cdn-icons-png.flaticon.com/128/900/900310.png" heigh="60px" width="60px"></img>
+<br/>
+<img src="https://cdn-icons-png.flaticon.com/128/2165/2165061.png"heigh="60px" width="60px"></img>
+<br/><br/>
+   <img src="https://cdn-icons-png.flaticon.com/128/535/535239.png" heigh="60px" width="60px"></img>
+   <br/>
 <img src="https://cdn-icons-png.flaticon.com/128/1927/1927656.png" heigh="60px" width="60px"></img>
   </div>
 <div class="column2">
@@ -60,8 +63,8 @@ Sri Jayawardenepura Kotte,sri lanka.
 </div>
 <div class="iframe">
 <iframe
- src="AIzaSyBERWn4JFQk-B9ZK91LXfuiIFNpuDfZoo4&40.7128,-74.0060" allowfullscreen>
-</iframe>
+ src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.8911652809784!2d79.90347757367157!3d6.903616793095686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259f73cc32a29%3A0x32d928d23b9faca3!2sArctic(Pvt)Ltd!5e0!3m2!1sen!2slk!4v1704474648219!5m2!1sen!2slk"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" > </iframe>
+
 </div>
 
 
