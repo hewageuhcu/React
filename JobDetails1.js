aa49b66e1d4524dd819a8da070b2e6069b05c28a
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-01-07 22:25:56 +0530

    Add files via upload

---
+++ JobDetails1.js
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import './JobDetails1.css';
+import { BrowserRouter as Browser,Router,Route,Switch } from 'react-router-dom';
+
+function JobDetails1() {
+    return (
+      <div>
+
+
+        {/* header */}
+<div class="tab-labels"></div>
+        
+        <div class="title">
+          Job Details
+        </div>
+
+        {/* search bar */}
+        <div class="column2">
+        
+        <form action="/search" method="get">
+  <input type="text" placeholder="Search..." name="search" />
+  <button type="submit">Search</button>
+</form>
+</div>
+       
+
+{/* dropdown menu */}
+<div class="column1">
+<div class="dropdown">
+  <button class="dropbtn">Filter By</button>
+  <div class="dropdown-content">
+    <a href="#Customer">Customer</a>
+    <a href="#Date">Date</a>
+    <a href="#Team Member">Team Member</a>
+    <a href="#Vehicle Number">Vehicle Number</a>
+    <a href="#To Be Invoiced">To Be Invoiced</a>
+    <a href="#Invoiced">Invoiced</a>
+
+  </div>
+  </div>
+  </div>
+
+  {/* tabs */}
+  {/* here, job id s should be displayed. that id s must come within the database. so after connecting the database, write the relevent code. */}
+  <div class="tabs"></div>
+  <div class="tabs"></div>
+  <div class="tabs"></div>
+  <div class="tabs"></div>
+  <div class="tabs"></div>
+  <div class="tabs"></div>
+  <div class="tabs"></div>
+<br/>
+  
+ 
+
+
+        {/* footer */}
+ <div class="footer">
+  <div class="footer-text">
+  <p>© 2023 • All Rights Reserved</p>
+  </div>
+ </div>
+</div>
+);
+  }
+
+
+export default JobDetails1;
