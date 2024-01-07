6cb1ee0ee4eae46b242045ac385d2bce5c768b31
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-01-07 13:48:13 +0530

    Add files via upload

---
+++ Welcome.js
@@ -0,0 +1,84 @@
+import logo from './logo.svg';
+import './Welcome.css';
+import { BrowserRouter as Browser,Router,Route,Switch } from 'react-router-dom';
+
+function Contact() {
+    return (
+      <div>
+
+
+       <div class="tab-labels"></div>
+      {/* welcome text */}
+
+     <div class="welcome-text">
+     
+      Welcome!
+     </div>
+<div class="tabs">
+  <div class="tab-text">
+ 
+  Employee Details
+   </div>
+     </div>
+   <div class="tabs">
+   <div class="tab-text">
+ Item details
+   </div>
+   <script>
+    
+   </script>
+   </div>
+
+   <div class="tabs">
+   <div class="tab-text">
+    Unit Details
+   </div>
+   </div>
+
+   <div class="tabs">
+   <div class="tab-text">
+    Vehicle Details
+   </div>
+   </div>
+
+   <div class="tabs">
+   <div class="tab-text">
+    Job Details
+   </div>
+   </div>
+   <div class="tabs">
+   <div class="tab-text">
+    Service Agreement
+   </div>
+   </div>
+
+   <div class="tabs">
+   <div class="tab-text">
+    Calender
+   </div>
+   </div>
+
+   <div class="tabs">
+   <div class="tab-text">
+    Schedule a Site Visit
+   </div>
+   </div>
+
+   <div class="tabs">
+   <div class="tab-text">
+    Job Allocation
+   </div>
+   </div>
+
+{/* footer */}
+<div class="footer">
+   <div class="footer-text">
+<p>© 2023 • All Rights Reserved</p>
+</div>
+</div>
+ </div>
+    );
+  }
+
+
+export default Contact;
